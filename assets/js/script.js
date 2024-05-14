ea08a7c5f7ea02f9f532bd4d12d6639fd895a3e0
AjmalKarim1 <ajmalkareem0099@gmail.com>
2024-05-14 04:30:09 -1200

    worked on responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,85 +4,71 @@ const toggleBtnIcon = document.querySelector(".toggle-nav-button i");
 const dropDownMenu = document.querySelector(".dropdown-menu");
 
 toggleBtn.onclick = function () {
-  // Toggle the 'open' class on the dropdown menu
   dropDownMenu.classList.toggle("open");
 
   // Check if dropdown menu has 'open' class
-  const isOpen = dropDownMenu.classList.contains("open");
+  toggleBtnIcon.classList.toggle("fa-xmark");
 
   // Toggle icon based on the dropdown state
-  toggleBtnIcon.className = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
 
-// photos slider
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+  },
+});
 
-// const initSlider = () => {
-//   const imageList = document.querySelector(".slider-wrapper .image-list");
-//   const slideButtons = document.querySelectorAll(
-//     ".slider-wrapper .slide-button"
-//   );
-//   const slideScrollbar = document.querySelector(
-//     ".photos-container .slider-scrollbar"
-//   );
-//   const scrollbarThumb = slideScrollbar.querySelector(".scrollbar-thumb");
-//   const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: false,
+  slidesPerGroup: 6,
+  grabCursor: true,
 
-//   scrollbarThumb.addEventListener("mousedown", (e) => {
-//     const startX = e.clientX;
-//     const thumbPosition = scrollbarThumb.offsetLeft;
-//     const handleMouseMove = (e) => {
-//       const deltaX = e.clientX - startX;
-//       const newThumbPosition = thumbPosition + deltaX;
-//       const maxThumbPosition =
-//         slideScrollbar.getBoundingClientRect().width -
-//         scrollbarThumb.offsetWidth;
-//       const boundedPosition = Math.max(
-//         0,
-//         Math.min(maxThumbPosition, newThumbPosition)
-//       );
-//       const scrollPosition =
-//         (boundedPosition / maxThumbPosition) * maxScrollLeft;
-
-//       scrollbarThumb.style.left = `${boundedPosition}px`;
-//       imageList.scrollLeft = scrollPosition;
-//     };
-
-//     const handleMouseup = () => {
-//       document.removeEventListener("mousemove", handleMouseMove);
-//       document.removeEventListener("mouseup", handleMouseup);
-//     };
-
-//     document.addEventListener("mousemove", handleMouseMove);
-//     document.addEventListener("mouseup", handleMouseup);
-//   });
-
-//   slideButtons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//       const direction = button.id === "prev-slide" ? -1 : 1;
-//       const scrollAmount = imageList.clientWidth * direction;
-//       imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
-//     });
-//   });
-
-//   const handleSlideButtons = () => {
-//     slideButtons[0].style.display =
-//       imageList.scrollLeft <= 0 ? "none" : "block";
-//     slideButtons[1].style.display =
-//       imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
-//   };
-
-//   const updateScrollThumbPosition = () => {
-//     const scrollPosition = imageList.scrollLeft;
-//     const thumbPosition =
-//       (scrollPosition / maxScrollLeft) *
-//       (slideScrollbar.clientWidth - scrollbarThumb.offsetWidth);
-//     scrollbarThumb.style.left = `${thumbPosition}px`;
-//   };
-
-//   imageList.addEventListener("scroll", () => {
-//     handleSlideButtons();
-//     updateScrollThumbPosition();
-//   });
-// };
-
-// window.addEventListener("load", initSlider);
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      slidesPerGroup: 6,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 3,
+      slidesPerGroup: 6,
+      spaceBetween: 40,
+    },
+    1050: {
+      slidesPerView: 4,
+      slidesPerGroup: 6,
+      spaceBetween: 40,
+    },
+  },
+});

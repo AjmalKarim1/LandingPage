0a52c36a39a9397b80ff2230db435a08ea96fb39
AjmalKarim1 <ajmalkareem0099@gmail.com>
2024-05-24 08:22:11 -1200

    made slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,3 +72,62 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+// indudial products
+var swiper = new Swiper(".mtSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: false,
+  slidesPerGroup: 6,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 4,
+      spaceBetween: 40,
+    },
+    320: {
+      slidesPerView: 2,
+      slidesPerGroup: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+// brands logo js
+var swiper = new Swiper(".brSwiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  loop: false,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      slidesPerGroup: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 6,
+      slidesPerGroup: 6,
+      spaceBetween: 30,
+    },
+  },
+});
